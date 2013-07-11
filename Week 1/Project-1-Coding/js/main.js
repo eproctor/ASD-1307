e29883c3260800806b5496895f977a92b6445095
E Proctor <eproctor@fullsail.edu>
2013-07-11 17:18:37 -0400

    Week1 Files

---
+++ Week 1/Project-1-Coding/js/main.js
@@ -0,0 +1,14 @@
+/* Name: Eugene Proctor :: Project 1 :: ASD 1307 */
+/*  ================JS File =========================  */
+
+
+
+
+
+
+// Validation for Home Page
+$('#home').on('pageinit', function(){
+	//code needed for home page goes here
+});	
+
+
