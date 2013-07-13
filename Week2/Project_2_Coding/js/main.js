d6d55502bcdefafcc4bd800ec57c3373967c6309
E Proctor <eproctor@fullsail.edu>
2013-07-12 22:49:15 -0400

    Week 2 Starter Files for the week

---
+++ Week2/Project_2_Coding/js/main.js
@@ -0,0 +1,43 @@
+/* Name: Eugene Proctor :: Project 1 :: ASD 1307 */
+/*  ================JS File =========================  */
+$(document).ready(function(){
+
+
+
+
+/*  ================ Home Page Pageinit =========================  */
+ 
+$('#home').on('pageinit', function(){
+	//code needed for home page goes here
+});	
+
+
+
+
+/*  ================ About Page Pageinit =========================  */
+
+$('#about').on('pageinit', function(){
+	//code needed for home page goes here
+});	
+
+
+
+
+/*  ================ Form Page Pageinit =========================  */
+
+$('#formPage').on('pageinit', function(){
+	//code needed for home page goes here
+});	
+
+
+
+
+
+/*  ================ Display Page Pageinit =========================  */
+
+$('#displayPage').on('pageinit', function(){
+	//code needed for home page goes here
+});	
+
+
+});
